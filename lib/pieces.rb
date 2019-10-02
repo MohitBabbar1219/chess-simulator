d7bfb62c8bcfd63f8a411a8cb96b1ad411bdb201
MohitBabbar1219 <mohitbabbar1219@gmail.com>
2019-10-03 00:07:37 +0530

    Add rook's valid moves and their specs

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -2,5 +2,9 @@ module Pieces
   module ValidMoves
     one_step_forward = ->(current_position, proposed_position) { current_position.add(Coordinate.new(0, 1)).eql?(proposed_position) }
     PAWN = [one_step_forward]
+
+    vertical = ->(current_position, proposed_position) { current_position.y_coordinate == proposed_position.y_coordinate }
+    horizontal = ->(current_position, proposed_position) { current_position.x_coordinate == proposed_position.x_coordinate }
+    ROOK = [vertical, horizontal]
   end
 end
--- spec/pieces_spec.rb
+++ spec/pieces_spec.rb
@@ -29,5 +29,39 @@ describe 'Pieces' do
         expect(a_pawn.position).to eql(a_position)
       end
     end
+
+    describe '::ROOK' do
+      it 'allows rook to move one step forward' do
+        a_position = Coordinate.new(1, 2)
+        a_rook = Piece.new(a_position, Piece::Set::WHITE, Pieces::ValidMoves::ROOK)
+        position_to_move_to = Coordinate.new(1, 3)
+        a_rook.move_to(position_to_move_to)
+        expect(a_rook.position).to eql(position_to_move_to)
+      end
+
+      it 'allows rook to move two steps backward' do
+        a_position = Coordinate.new(1, 2)
+        a_rook = Piece.new(a_position, Piece::Set::WHITE, Pieces::ValidMoves::ROOK)
+        position_to_move_to = Coordinate.new(1, 0)
+        a_rook.move_to(position_to_move_to)
+        expect(a_rook.position).to eql(position_to_move_to)
+      end
+
+      it 'allows rook to move three steps right' do
+        a_position = Coordinate.new(1, 2)
+        a_rook = Piece.new(a_position, Piece::Set::WHITE, Pieces::ValidMoves::ROOK)
+        position_to_move_to = Coordinate.new(4, 2)
+        a_rook.move_to(position_to_move_to)
+        expect(a_rook.position).to eql(position_to_move_to)
+      end
+
+      it 'does not allow rook to move diagonal' do
+        a_position = Coordinate.new(1, 2)
+        a_rook = Piece.new(a_position, Piece::Set::WHITE, Pieces::ValidMoves::ROOK)
+        position_to_move_to = Coordinate.new(2, 3)
+        a_rook.move_to(position_to_move_to)
+        expect(a_rook.position).to eql(a_position)
+      end
+    end
   end
 end
