e00c643838097f1b52ca63af8cfe7a26389671ac
MohitBabbar1219 <mohitbabbar1219@gmail.com>
2019-10-02 23:48:43 +0530

    Remove set factory methods and make constructor public

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,17 +1,15 @@
 class Piece
   attr_reader :set, :position
 
+  def initialize(position, set, valid_moves)
+    @position = position
+    @set = set
+    @valid_moves = valid_moves
+  end
+
   module Set
     BLACK = :black
     WHITE = :white
-  end
-
-  def self.black(position, valid_moves)
-    Piece.new(position, Set::BLACK, valid_moves)
-  end
-
-  def self.white(position, valid_moves)
-    Piece.new(position, Set::WHITE, valid_moves)
   end
 
   def move_to(position)
@@ -22,11 +20,5 @@ class Piece
 
   def is_move_valid(proposed_position)
     @valid_moves.select { |move| move.call(@position, proposed_position) }.length > 0
-  end
-
-  def initialize(position, set, valid_moves)
-    @position = position
-    @set = set
-    @valid_moves = valid_moves
   end
 end
